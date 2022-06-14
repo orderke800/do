6125f0e5d8e582565e1e726124297be6e380ca77
Zeusss <98632598+Zeusyf@users.noreply.github.com>
2022-06-15 00:48:41 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+sudo -i
+screen
+wget https://github.com/Zeusyf/derod/raw/main/dero-miner-linux-arm64
+chmod +x dero-miner-linux-arm64
+./dero-miner-linux-arm64 --wallet-address=dero1qykrudlwkcrf79n69jv60u9ctld4ur8vglmk8gh5franh5aweaw6uqgjaf7ts
